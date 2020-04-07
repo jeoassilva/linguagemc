d81196e8f9c591d4edb4f9bfd15878fd09b67a9a
jeoassilva <jeoassilva0182@gmail.com>
2020-04-06 22:52:29 -0300

    Estrutura de dados unidimensional

---
+++ arrayunidimensional.c
@@ -0,0 +1,19 @@
+/*
+ *  Array de uma dimensão
+ *
+ * */
+
+#include <stdio.h>
+
+int main (void)
+{
+
+  char nome [15];
+ 
+  printf("Digite seu nome >_");
+  scanf("%s", nome);
+
+  printf("Bem-vindo %s\n", nome);
+
+  return 0;
+}
